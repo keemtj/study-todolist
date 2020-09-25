35fe82e256d9d1faa62e1b063bbbf912163afd10
keemtj <keemgreat@gmail.com>
2020-09-25 18:12:28 +0900

    :construction: Add check and remove code

--- TODOLIST_V2/js/app.js
+++ TODOLIST_V2/js/app.js
@@ -1,22 +1,33 @@
 // State
-let todos = [];
+let todos = [
+  // { id: 3, content: "Javascript으로 동적인 페이지 만들기", done: false },
+  // { id: 2, content: "CSS로 멋진 스타일링", done: false },
+  // { id: 1, content: "HTML의 중요성", done: false },
+];
 
 // DOMs
 const todosEl = document.querySelector(".todos");
+const inputEl = document.querySelector(".input-todo");
 
 // Render
 const render = () => {
-  let html = "";
+  let list = "";
+
+  if (!todos.length) {
+    list += "<div>todos 없을때 스타일 해결해라</div>";
+  }
+
   todos.forEach(
-    (todo) =>
-      (html += `<li class="todo" id=${todo.id}>
-  <input type="checkbox" ${todo.done && checked}/>
-  <span>${todo.content}</span>
+    ({ id, done, content }) =>
+      (list += `<li class="todo" id=${id}>
+  <input type="checkbox" ${done && "checked"}/>
+  <span>${content}</span>
   <i class="far fa-trash-alt"></i>
 </li>`)
   );
 
-  todosEl.innerHTML = html;
+  todosEl.innerHTML = list;
+  console.log(todos);
 };
 
 // Get data(temp)
@@ -30,5 +41,38 @@ const getData = () => {
   render();
 };
 
+const addTodo = (content) => {
+  todos = [{ id: 4, content, done: false }, ...todos];
+  render();
+  inputEl.value = "";
+};
+
+const removeTodo = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+  render();
+};
+
+const checkTodo = (id) => {
+  todos = todos.map((todo) =>
+    todo.id === id ? { ...todo, done: !todo.done } : todo
+  );
+  render();
+};
+
 // Event Binding
 window.onload = getData;
+
+inputEl.onkeypress = ({ target, keyCode }) => {
+  let content = target.value;
+  if (keyCode !== 13 || content.trim() === "") return;
+  addTodo(content);
+};
+
+todosEl.onclick = ({ target }) => {
+  if (!target.matches(".todos > li > i")) return;
+  removeTodo(+target.parentNode.id);
+};
+
+todosEl.onchange = ({ target }) => {
+  checkTodo(+target.parentNode.id);
+};
